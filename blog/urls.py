9885ca5ebd93a43ec6fe9d8789542dc6a4da6361
nurbekomar <114141734+nurbekomar@users.noreply.github.com>
2023-09-05 17:44:10 +0600

    add menu

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from blog.views import *
 
 urlpatterns = [
-    path("", index),
-    path("about/", about)
+    path("", index, name="home"),
+    path("blog/", blog, name="blog"),
+    path("about/", about, name="about"),
+    path("contact/", contact, name="contact"),
+    path("post/<int:post_id>", show_post, name="post")
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,36 @@ from django.http import HttpResponse, HttpResponseNotFound
 from blog.models import *
 # Create your views here.
 
-menu = ["О Сайте", "Добавить", "Удалить", "Сохранить"]
+menu = [{'title': "STEM", 'url_name': "home"},
+        {'title': "Blog", 'url_name': "blog"},
+        {'title': "ABOUT", 'url_name': "about"},
+        {'title': "Contact", 'url_name': "contact"}]
 
 
 def index(request):
     posts = Blog.objects.all()
-    return render(request, "blog/index.html", {"posts": posts, "menu": menu, "title": "Main Page"})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'STEM'
+    }
+    return render(request, "blog/index.html", context=context)
+
+
+def blog(request):
+    return HttpResponse("BLOG")
 
 
 def about(request):
     return render(request, "blog/about.html", {"menu": menu, "title": "About"})
+
+
+def contact(request):
+    return HttpResponse("CONTACT")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Post = {post_id}")
 
 
 def pageNotFound(request, exception):
