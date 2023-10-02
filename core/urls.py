7dfbff6d522e71b56fe3d02ac8930e8be66fd5e9
nurbekomar <114141734+nurbekomar@users.noreply.github.com>
2023-10-02 16:02:48 +0600

    update container

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('tinymce/', include('tinymce.urls')),
 ]
 
 if settings.DEBUG:
